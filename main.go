b1887d681bd95a23de604e7102a611d5672a2576
alvaroart <alvaro.rosa1985@gmail.com>
2024-03-13 21:50:35 +0000

    TODO inmemory

--- main.go
+++ main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
+var DB TodoDatabase
+
 type TodoDatabase interface {
 	RetrieveTodoList() []Todo
+	CreateTodo(t Todo)
 }
 
 type InMemoryTodoDatabase struct {
+	todoList map[uuid.UUID]Todo
 }
 
 func NewInMemoryTodoDatabase() InMemoryTodoDatabase {
-	return InMemoryTodoDatabase{}
+	result := InMemoryTodoDatabase{}
+	result.todoList = make(map[uuid.UUID]Todo)
+	return result
 }
 
 func (imdb *InMemoryTodoDatabase) RetrieveTodoList() []Todo {
-	return []Todo{
-		{UUID: uuid.New(), Title: "Zeh cade vc !"},
-		{UUID: uuid.New(), Title: "Title 2"},
-		{UUID: uuid.New(), Title: "Title 3"},
+
+	todos := make([]Todo, 0)
+	for key, todo := range imdb.todoList {
+		fmt.Printf("%s", key.String())
+		todos = append(todos, todo)
 	}
+	return todos
+
+}
+
+func (imdb *InMemoryTodoDatabase) CreateTodo(t Todo) {
+	uniqueId := t.UUID
+	// t := Todo{UUID: uniqueId, Title: "Zeh cade vc !"}
+	imdb.todoList[uniqueId] = t
+
 }
 
 type Todo struct {
@@ -37,13 +54,20 @@ type TodosResponse struct {
 
 func main() {
 	r := gin.Default()
+	DB := NewInMemoryTodoDatabase()
 	r.GET("/todos", func(c *gin.Context) {
-		db := NewInMemoryTodoDatabase()
 
 		response := TodosResponse{
-			Items: db.RetrieveTodoList(),
+			Items: DB.RetrieveTodoList(),
 		} // Sua lista de itens aqui
 		c.JSON(http.StatusOK, response)
 	})
+
+	r.POST("/todos", func(c *gin.Context) {
+		t := Todo{UUID: uuid.New(), Title: "Zeh cade vc !"}
+		DB.CreateTodo(t)
+		c.JSON(http.StatusNoContent, gin.H{})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
